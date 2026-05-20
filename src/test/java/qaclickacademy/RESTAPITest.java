36326061b296d406138c25ec5dd443b79ebd842b
Mithun <mithuneonian@gmail.com>
2026-05-20 17:50:16 +0530

    this is the first time i try to push code from GitX file

---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,44 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+//rahulonlinetutor@gmail.com
+public class RESTAPITest {
+
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+		//feere
+		//ffgf
+		//fgf
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+		System.out.println("postJira7");
+		
+		System.out.println("Postjira 21");
+		System.out.println("Postjira 22");
+		System.out.println("Postjira 23");
+		
+		
+		
+		
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+		
+		
+	}
+	@Test
+	public void deleteTwitter12()
+	{
+		System.out.println("deleteTwddtter");
+		
+		
+	}
+}
