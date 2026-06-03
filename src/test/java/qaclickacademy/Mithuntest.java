ca6f38144a4c09930af8499232f5095056a41847
Mithun <mithuneonian@gmail.com>
2026-06-03 17:27:50 +0530

    Final commit from american guy - final deployment

---
+++ src/test/java/qaclickacademy/Mithuntest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+public class Mithuntest
+{
+public void first()
+{
+	
+	System.out.println("this is first");
+}
+public void Second()
+{
+	
+	System.out.println("this is Second");
+}
+public void Third()
+{
+	
+	System.out.println("this is Third");
+}
+}
